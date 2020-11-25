6d2d8fd65b3a68261c3b26824591ae4ce555d8d3
ranckprogram <1474574022@qq.com>
2020-11-25 15:39:17 +0800

    配置permissions 后可以访问浏览器的一些资源

--- popup.js
+++ popup.js
@@ -1,28 +1,21 @@
-// function onAnchorClick(event) {
-//   chrome.tabs.create({ url: event.srcElement.href });
-//   return false;
-// }
 
-// // Given an array of URLs, build a DOM list of these URLs in the
-// // browser action popup.
-// function buildPopupDom(mostVisitedURLs) {
-//   var popupDiv = document.getElementById("mostVisited_div");
-//   var ol = popupDiv.appendChild(document.createElement("ol"));
+function onAnchorClick(event) {
+  chrome.tabs.create({ url: event.srcElement.href });
+  return false;
+}
 
-//   for (var i = 0; i < mostVisitedURLs.length; i++) {
-//     var li = ol.appendChild(document.createElement("li"));
-//     var a = li.appendChild(document.createElement("a"));
-//     a.href = mostVisitedURLs[i].url;
-//     a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
-//     a.addEventListener("click", onAnchorClick);
-//   }
-// }
+function buildPopupDom(mostVisitedURLs) {
+  var popupDiv = document.getElementById('mostVisited_div');
+  var ol = popupDiv.appendChild(document.createElement('ol'));
 
-// console.log(chrome, 666);
+  for (var i = 0; i < mostVisitedURLs.length; i++) {
+    var li = ol.appendChild(document.createElement('li'));
+    var a = li.appendChild(document.createElement('a'));
+    a.href = mostVisitedURLs[i].url;
+    a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
+    a.addEventListener('click', onAnchorClick);
+  }
+}
 
-// function buildPopupDom (mostVisitedURLs) {
-var popupDiv = document.getElementById("mostVisited_div");
-popupDiv.innerHTML = JSON.stringify(11);
-// }
 
-// chrome.topSites.get(buildPopupDom);
+chrome.topSites.get(buildPopupDom);
