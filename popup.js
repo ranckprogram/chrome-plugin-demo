97d4d60cfc6a1c66b019648f83fc02c80b24d914
ranckprogram <1474574022@qq.com>
2020-11-25 16:02:11 +0800

    脚本注入

--- popup.js
+++ popup.js
@@ -1,21 +1,19 @@
-
 function onAnchorClick(event) {
   chrome.tabs.create({ url: event.srcElement.href });
   return false;
 }
 
 function buildPopupDom(mostVisitedURLs) {
-  var popupDiv = document.getElementById('mostVisited_div');
-  var ol = popupDiv.appendChild(document.createElement('ol'));
+  var popupDiv = document.getElementById("mostVisited_div");
+  var ol = popupDiv.appendChild(document.createElement("ol"));
 
   for (var i = 0; i < mostVisitedURLs.length; i++) {
-    var li = ol.appendChild(document.createElement('li'));
-    var a = li.appendChild(document.createElement('a'));
+    var li = ol.appendChild(document.createElement("li"));
+    var a = li.appendChild(document.createElement("a"));
     a.href = mostVisitedURLs[i].url;
     a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
-    a.addEventListener('click', onAnchorClick);
+    a.addEventListener("click", onAnchorClick);
   }
 }
-
 
 chrome.topSites.get(buildPopupDom);
---
+++ web/tool.js
@@ -0,0 +1,13 @@
+// chrome.tab.query(
+//   {
+//     active: true,
+//     currentWindow: true,
+//   },
+//   function (tabs) {
+//     chrome.executeScript(tabs[0].id, {
+//       code: "document.style.color = 'red'",
+//     });
+//   }
+// );
+
+document.getElementById('su').value = "查查查"
