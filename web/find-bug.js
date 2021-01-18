572dcd2e0a9c6fd90f9794625e3d2587f5bfe469
ranckprogram <1474574022@qq.com>
2021-01-18 11:26:37 +0800

    ei

--- web/find-bug.js
+++ web/find-bug.js
@@ -1,2 +1,21 @@
-console.log("我是找bug的chrome插件")
+console.log("我是找bug的chrome插件");
 
+function IFindBuged() {
+  console.log("我找到bug了");
+}
+
+this.IFindBuged = IFindBuged;
+console.log(this);
+console.log(window);
+
+setTimeout(() => {
+  console.log("bbbb==========");
+  console.log(this);
+}, 3000);
+
+chrome.contextMenus.create({
+  title: "测试右键菜单",
+  onclick: function () {
+    alert("您点击了右键菜单！");
+  },
+});
